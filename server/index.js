295934717c7b9aeb805f786813caf272c2a2eea7
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-07-09 23:18:48 +0100

    update server port

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import middleware from "./middleware.js";
 import routes from "./routes.js";
+import env from "dotenv";
 
+env.config();
 // Express JS
 const app = express();
 // PORT
-const port = 3000;
+const port = process.env.PORT;
 
 // MIDDLEWARE
 middleware(app);
