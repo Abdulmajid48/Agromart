aa5064a2a0c5739bdd906ea798118f5a0e550e7d
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-07-14 17:21:29 +0100

    client side--------

--- server/components/middleware.js
+++ server/components/middleware.js
@@ -13,7 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 
 // Get the directory name of the current module
 const __dirname = path.dirname(__filename);
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/index.html"));
+  });
 // Initialize MemoryStore
 const MemoryStore = createMemoryStore(session);
 
--- server/components/routes.js
+++ server/components/routes.js
@@ -1,13 +1,11 @@
 import passport from "./authroute.js";
 import { register } from "./authroute.js";
 import env from "dotenv";
-import path from "path";
+
 
 env.config();
 const routes = (app) => {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/index.html"));
-  });
+
 
   // Rerender login page
   app.get("/login", (req, res) => {
