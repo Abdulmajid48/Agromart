16013928bae76797a1651d51e9ccc80a541205cf
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-07-14 16:56:42 +0100

    client side-----

--- server/components/middleware.js
+++ server/components/middleware.js
@@ -1,13 +1,19 @@
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import session from "express-session";
 import passport from "passport";
 import env from "dotenv";
 import createMemoryStore from "memorystore";
-import path from "path"
+import path from "path";
+import { fileURLToPath } from "url";
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client')));
+// Convert import.meta.url to __filename
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name of the current module
+const __dirname = path.dirname(__filename);
+
 // Initialize MemoryStore
 const MemoryStore = createMemoryStore(session);
 
@@ -15,6 +21,8 @@ env.config();
 
 // MIDDLEWARE
 const middleware = (app) => {
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, "client")));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(
     cors({
