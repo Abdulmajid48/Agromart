57a807edf89cf7969c29aa2ffc8ea263ad37411e
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-07-14 17:08:46 +0100

    client side------

--- server/components/middleware.js
+++ server/components/middleware.js
@@ -5,14 +5,6 @@ import session from "express-session";
 import passport from "passport";
 import env from "dotenv";
 import createMemoryStore from "memorystore";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Convert import.meta.url to __filename
-const __filename = fileURLToPath(import.meta.url);
-
-// Get the directory name of the current module
-const __dirname = path.dirname(__filename);
 
 // Initialize MemoryStore
 const MemoryStore = createMemoryStore(session);
@@ -21,8 +13,6 @@ env.config();
 
 // MIDDLEWARE
 const middleware = (app) => {
-  // Serve static files from the React app
-  app.use(express.static(path.join(__dirname, "client")));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(
     cors({
