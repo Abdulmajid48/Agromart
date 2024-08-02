55440d182d8737a6759d078a3d75b943ac67ea14
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-08-02 17:59:46 +0100

    fix authcontext

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from "./AuthContext";
+import { AuthContext } from "../components/AuthContext";
 
 const Products = () => {
   const { user } = useContext(AuthContext);
