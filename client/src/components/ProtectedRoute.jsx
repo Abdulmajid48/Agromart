1ca4b750689873495ba6f5c4d2d1c5b1a419dbc8
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-08-02 20:59:46 +0100

    fix autht

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,34 +1,32 @@
-// // import { useContext, useEffect, useState } from "react";
-// // import { Navigate } from "react-router-dom";
-// // import { GuestUser } from "./Signpage";
-
-// // const ProtectedRoute = ({ children }) => {
-// //   const { guest } = useContext(GuestUser);
-// //   const [isAuthenticated, setIsAuthenticated] = useState(null);
-
-// //   useEffect(() => {
-// //     const checkAuth = async () => {
-// //       try {
-// //         if (!guest) return setIsAuthenticated(false);
-// //         return setIsAuthenticated(true);
-// //       } catch (error) {
-// //         console.log(error);
-// //       }
-// //     };
-// //     checkAuth();
-// //   }, [guest]);
-
-// //   if (isAuthenticated === false) return <div>Loading...</div>;
-// //   return isAuthenticated ? children : <Navigate to="/login" />;
-// // };
-// import { useContext } from "react";
+// import { useContext, useEffect, useState } from "react";
 // import { Navigate } from "react-router-dom";
-// //import { AuthContext } from "./AuthContext";
+// import { GuestUser } from "./Signpage";
 
 // const ProtectedRoute = ({ children }) => {
-//   //const { user } = useContext(AuthContext);
+//   const { guest } = useContext(GuestUser);
+//   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-//   if (!user) return <Navigate to="/login" />;
-//   return children;
+//   useEffect(() => {
+//     const checkAuth = async () => {
+//       try {
+//         if (!guest) return setIsAuthenticated(false);
+//         return setIsAuthenticated(true);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     checkAuth();
+//   }, [guest]);
+
+//   if (isAuthenticated === false) return <div>Loading...</div>;
+//   return isAuthenticated ? children : <Navigate to="/login" />;
 // };
-// export default ProtectedRoute;
+
+//import { AuthContext } from "./AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+  //const { user } = useContext(AuthContext);
+
+  return children;
+};
+export default ProtectedRoute;
