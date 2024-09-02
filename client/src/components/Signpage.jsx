e351dc68110eba6b7619260b78fd411e47253286
Abdul-Majid Aladejana <aladejanamajid48@gmail.com>
2024-09-03 00:25:30 +0100

    add url

--- client/src/components/Signpage.jsx
+++ client/src/components/Signpage.jsx
@@ -38,7 +38,7 @@ function Signpage(props) {
       return !preValue;
     });
   }
-  const url = import.meta.VITE_BACKEND;
+  const url = "https://agromart-db.vercel.app";
   const config = {
     withCredentials: true,
     headers: {
